9c04cf40a8ff8844465a051318ee99fa8d19e213
Priyansh Lohiya <plohiya@griddynamics.com>
2026-02-06 11:25:45 +0530

    Downgraded sprng version to accomodate eureca servies

--- product-service/src/test/java/plohiya/product_service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/plohiya/product_service/ProductServiceApplicationTests.java
@@ -1,23 +1,26 @@
 package plohiya.product_service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.mongodb.MongoDBContainer;
+
 
 import plohiya.product_service.dto.ProductRequest;
 import plohiya.product_service.repository.ProductRepository;
-import tools.jackson.databind.ObjectMapper;
+
 
 import java.math.BigDecimal;
 
